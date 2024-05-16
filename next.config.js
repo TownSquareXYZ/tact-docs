44e9e1a7db9f502623f60bd7062fdecc62923023
sansx <646924078@qq.com>
2024-05-16 16:26:52 +0800

    add i18n config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,40 @@
-import { getHighlighter, BUNDLED_LANGUAGES } from 'shiki';
-import nextra from 'nextra';
-import fs from 'fs';
+import { getHighlighter, BUNDLED_LANGUAGES } from "shiki";
+import nextra from "nextra";
+import fs from "fs";
 
-const grammar_tact = JSON.parse(fs.readFileSync('./grammars/grammar-tact.json', 'utf-8'));
-const grammar_func = JSON.parse(fs.readFileSync('./grammars/grammar-func.json', 'utf-8'));
-const grammar_ohm = JSON.parse(fs.readFileSync('./grammars/grammar-ohm.json', 'utf-8'));
+const grammar_tact = JSON.parse(
+  fs.readFileSync("./grammars/grammar-tact.json", "utf-8")
+);
+const grammar_func = JSON.parse(
+  fs.readFileSync("./grammars/grammar-func.json", "utf-8")
+);
+const grammar_ohm = JSON.parse(
+  fs.readFileSync("./grammars/grammar-ohm.json", "utf-8")
+);
 
 const rehypePrettyCodeOptions = {
-  getHighlighter: options => {
+  getHighlighter: (options) => {
     return getHighlighter({
       ...options,
       langs: [
         ...BUNDLED_LANGUAGES,
         {
-          id: 'tact',
-          scopeName: 'source.tact',
+          id: "tact",
+          scopeName: "source.tact",
           grammar: grammar_tact,
-          aliases: ['tact'],
+          aliases: ["tact"],
         },
         {
-          id: 'func',
-          scopeName: 'source.func',
+          id: "func",
+          scopeName: "source.func",
           grammar: grammar_func,
-          aliases: ['func'],
+          aliases: ["func"],
         },
         {
-          id: 'ohm',
-          scopeName: 'source.ohm',
+          id: "ohm",
+          scopeName: "source.ohm",
           grammar: grammar_ohm,
-          aliases: ['ohm'],
+          aliases: ["ohm"],
         },
       ],
     });
@@ -39,13 +45,13 @@ const rehypePrettyCodeOptions = {
  * @type {import('next').NextConfig}
  */
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
   mdxOptions: {
-    rehypePrettyCodeOptions
+    rehypePrettyCodeOptions,
   },
   latex: true,
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
 });
 
 /**
@@ -53,7 +59,7 @@ const withNextra = nextra({
  */
 export default withNextra({
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   // i18n: {
   //   // locales: ['default', 'en', 'zh'],
@@ -61,18 +67,29 @@ export default withNextra({
   //   defaultLocale: 'default',
   //   // localeDetection: false,
   // },
+  // { locale: "en", text: "English" },
+  //   { locale: "zh-CN", text: "中文" },
+  //   { locale: "ru", text: "Русский" },
+  //   { locale: "ko", text: "한국어" },
+  //   { locale: "pl", text: "Polski" },
+  //   { locale: "uk", text: "Українська" },
+  i18n: {
+    locales: ['en', 'zh-CN', 'ru', 'ko', 'pl', 'uk'],
+    defaultLocale: "en",
+    localeDetection: false,
+  },
   webpack(config) {
-    const allowedSvgRegex = /components\/icons\/.+\.svg$/
+    const allowedSvgRegex = /components\/icons\/.+\.svg$/;
 
-    const fileLoaderRule = config.module.rules.find(rule =>
-      rule.test?.test?.('.svg')
-    )
-    fileLoaderRule.exclude = allowedSvgRegex
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+    fileLoaderRule.exclude = allowedSvgRegex;
 
     config.module.rules.push({
       test: allowedSvgRegex,
-      use: ['@svgr/webpack']
-    })
-    return config
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 // import '../styles.css'
+import { NextIntlClientProvider } from "next-intl";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <NextIntlClientProvider
+      locale={router.locale}
+      timeZone="Europe/Vienna"
+      messages={pageProps.messages}
+    >
+      <Component {...pageProps} />
+    </NextIntlClientProvider>
+  );
 }
